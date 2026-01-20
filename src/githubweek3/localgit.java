f6748feb5117a3fc44167b073974d1658b9ddda9
Sandeep <Sandeep@10.48.134.34>
2026-01-20 12:48:29 -0500

    print hello

--- src/githubweek3/localgit.java
+++ src/githubweek3/localgit.java
@@ -9,7 +9,7 @@ package githubweek3;
  * @author Sandeep
  */
 public class localgit {
-    private int age;
-    private int data;
-    
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
 }
