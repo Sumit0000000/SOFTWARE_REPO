f441daaf6d3bc93c3f7045ce05c10cc3bfcc8dae
Sumit0000000 <128108789+Sumit0000000@users.noreply.github.com>
2026-01-20 13:07:29 -0500

    Update localgit.java
    print something else

--- src/githubweek3/localgit.java
+++ src/githubweek3/localgit.java
@@ -11,5 +11,7 @@ package githubweek3;
 public class localgit {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("hello");
     }
 }
+
