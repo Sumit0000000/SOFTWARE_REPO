7d14f5acfc13477e646583e9021a4369e5034787
Sandeep <Sandeep@LAPTOP-AL2P5UE5>
2026-01-20 22:01:35 -0500

    this is last commit and push

--- src/ice1/ICE1.java
+++ src/ice1/ICE1.java
@@ -17,7 +17,32 @@ public class ICE1 {
         System.out.println("Hello! This is my first inclass exercise-1");
         System.out.println("Branch and clone are created");
         System.out.println("changes are made on github");
+        
+        String[] cards = new String[7];
+        cards[0] = "Ace";
+        cards[1] = "King";
+        cards[2] = "Queen";
+        cards[3] = "Jack";
+        cards[4] = "Ten";
+        cards[5] = "Nine";
+        cards[6] = "Eight";
 
+        String luckyCard = "Queen";
+
+        boolean found = false;
+
+        for (int i = 0; i < 7; i++) {
+            if (cards[i].equals(luckyCard)) {
+                found = true;
+            }
+        }
+
+        if (found) {
+            System.out.println("WIN");
+        } else {
+            System.out.println("LOSE");
+        }
+        
     }
 }
 
